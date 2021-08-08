714386e51101ba4b5e61e31201f2f34f387fb608
Nikita <facelessmay@gmail.com>
2021-08-08 21:05:44 +0700

    add search component

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -3,25 +3,45 @@
     <v-text-field
       class="flex flex-grow-0"
       v-model="cityName"
-      v-on:keyup.enter="getForecast"
+      v-on:keyup.enter="get"
       prepend-icon="mdi-city-variant-outline"
       label="Введите город..."
       :rules="rules"
     >
     </v-text-field>
-    <v-btn
-      class="ml-10"
-      @click="getForecast"
-      @keyup.enter="getForecast"
-      :loading="isWeatherDataLoading"
-    >Найти</v-btn
-    >
+    <app-search-button
+      @clickGetForecast="get"
+      :is-weather-loading="isWeatherLoading"
+      :disabled="!!cityName.length"
+    ></app-search-button>
   </div>
 </template>
 
 <script>
+import AppSearchButton from "./AppSearchButton";
+
 export default {
   name: "AppSearchField",
+  components: {
+    AppSearchButton,
+  },
+  props: {
+    isWeatherLoading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      cityName: "",
+      rules: [(val) => (val || "").length > 0 || "Заполните поле"], // валидация поля
+    };
+  },
+  methods: {
+    get() {
+      this.$emit("get-forecast", this.cityName);
+    },
+  },
 };
 </script>
 
