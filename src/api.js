39e43b5a13b82a6cb06a4577338848f44b5668b2
Nikita <facelessmay@gmail.com>
2021-08-08 21:08:07 +0700

    api layout implem

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const weatherID = "7ddbbac4414598a0582efd0c12096e75";
-// celsius, m/s and etc
-const typeUnits = "metric";
+
+// небольшая прослойка для запросов
 
 const apiClient = axios.create({
-  baseURL: "https://api.openweathermap.org/data/2.5/forecast?",
+  baseURL: "https://api.openweathermap.org/data/2.5",
   withCredentials: false,
   headers: {
     accept: "application/json",
@@ -13,9 +13,14 @@ const apiClient = axios.create({
 });
 
 export default {
-  async getWeatherForecast(city) {
+  async getWeatherCurrent(city, units = "metric") {
     return await apiClient.get(
-      `q=${city}&units=${typeUnits}&lang=ru&appid=${weatherID}`
+      `weather?q=${city}&units=${units}&lang=ru&appid=${weatherID}`
+    );
+  },
+  async oneCallForecast(lat, lon, units = "metric") {
+    return await apiClient.get(
+      `onecall?lat=${lat}&lon=${lon}&units=${units}&lang=ru&exclude=minutely,hourly,alerts,current&appid=${weatherID}`
     );
   },
 };
