ff6251c4405d2ffaf05864b67b308fa3da083d27
Nikita <facelessmay@gmail.com>
2021-08-08 21:06:29 +0700

    add search buttom component

--- src/components/AppSearchButton.vue
+++ src/components/AppSearchButton.vue
@@ -1,13 +1,27 @@
 <template>
-$END$
+  <v-btn
+    :disabled="!disabled"
+    class="ml-10"
+    @click="$emit('clickGetForecast')"
+    :loading="isWeatherLoading"
+    >Найти
+    <v-icon right size="25px"> mdi-cloud-search-outline </v-icon>
+  </v-btn>
 </template>
 
 <script>
 export default {
-name: "AppSearchButton"
-}
+  name: "AppSearchButton",
+  props: {
+    isWeatherLoading: {
+      type: Boolean,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
