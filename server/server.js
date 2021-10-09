006c4cf518796f25b3e17cd6934937e8c3b802ca
Nikita Likhachev <facelessmay@gmail.com>
2021-10-09 16:55:06 +0700

    restructure proj

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "/dist")));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 const port = process.env.PORT || 8080;
