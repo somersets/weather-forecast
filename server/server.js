64b1c0b8448ad715d2dc9d1c955f5f1bbfe5f46f
Nikita Likhachev <facelessmay@gmail.com>
2021-10-09 23:47:50 +0700

    fix: correct path

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
