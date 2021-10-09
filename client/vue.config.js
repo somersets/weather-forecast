1e485ac719e97d2d5c6cf71ba12037a66a189c99
Nikita Likhachev <facelessmay@gmail.com>
2021-10-09 17:37:48 +0700

    fix: path output build dir

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
 module.exports = {
   transpileDependencies: ["vuetify"],
-  outputDir: path.resolve(__dirname, "./server/public"),
+  outputDir: path.resolve(__dirname, "../server/public"),
 };
